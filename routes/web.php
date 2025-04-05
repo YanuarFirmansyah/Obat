e56944011612706665d99e4435e40e5587134813
YanuarFirmansyah <januar.firmansyah16@gmail.com>
2025-04-05 19:46:12 +0700

    tugas 3

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.main');
+});
+
+Route::get('/dokter', function () {
+    return view('list-dokter');
+});
+
+Route::get('/obat', function () {
+    return view('list-obat');
 });
